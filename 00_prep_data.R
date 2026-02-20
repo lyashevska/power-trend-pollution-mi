12b60eac508c29c21967838abfbe0ab39b89ff4a
lyashevska <olga.lyashevska@gmail.com>
2026-02-20 18:53:59 +0100

    refactor data prep

--- 00_prep_data.R
+++ 00_prep_data.R
@@ -10,40 +10,12 @@
 ###################################
 
 library(dplyr)
+library(readr)
 
-# set working directory to source file location
-setwd(system("pwd", intern = T))
-
-data <- read.csv("juveniles.csv")
-
-cat("Raw data dim:", dim(data), "\n")
-cat("Year range:", range(data$Year), "\n")
-
-# keep columns of interest
-cnames <-
-  c(
-    "Year",
-    "Sex",
-    "Latitude",
-    "Longitude",
-    "AreaAU",
-    "Body.Weight..kg.",
-    "Age..yr.",
-    "Length",
-    "AreaAA",
-    "sumCBs1"
-  )
-
-data <- data[,cnames]
-
-data <- subset(
-  data,
-  !is.na(Body.Weight..kg.) & !is.na(Length)
-)
-
-# rename columns
-
-data <- data %>%
+data <- read_csv("csv/juveniles.csv", show_col_types = FALSE) %>%
+  select(Year, Sex, Latitude, Longitude, AreaAU, Body.Weight..kg., 
+         Age..yr., Length, AreaAA, sumCBs1) %>%
+  filter(!is.na(Body.Weight..kg.), !is.na(Length)) %>%
   rename(
     HP.AU = AreaAU,
     Ospar.AA = AreaAA,
@@ -53,13 +25,13 @@ data <- data %>%
   )
 
 cat("Final data dim:", dim(data), "\n")
+# Final data dim: 415 10
 
 # relative body weight
 logwl <- lm(log(Weight) ~ log(Length),
             data = data,
             na.action = na.omit)
 
-summary(logwl)
 data$Rel.body.wt <- logwl$residuals
 
-saveRDS(data, file = "data.rds")
+saveRDS(data, file = "rds/data.rds")
